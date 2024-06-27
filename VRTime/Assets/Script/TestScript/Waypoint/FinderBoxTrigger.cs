61a3f8a05c162460db34ebf3bfc15fd38b2865fb
shindongju <sdj3701@naver.com>
2024-06-27 15:12:56 +0900

    Feat : 음성 파일 재생
    음성 파일을 경로를 찾아서 재생 시킴

--- VRTime/Assets/Script/TestScript/Waypoint/FinderBoxTrigger.cs
+++ VRTime/Assets/Script/TestScript/Waypoint/FinderBoxTrigger.cs
@@ -16,7 +16,13 @@ public class FinderBoxTrigger : MonoBehaviour
     {
         if(other.gameObject.name == "Player")
         {
-            // TODO : 추후 변경해야하는 코드 2줄
+            //음성 경로로 찾아서 넣어서 재생
+            AudioSource audioSource = other.gameObject.GetComponentInChildren<AudioSource>();
+            AudioClip audioClip = Resources.Load<AudioClip>("Sound/rightnow");
+            audioSource.clip = audioClip;
+            audioSource.Play();
+
+            // TODO : 추후 변경해야하는 코드 2줄 이거를 딕션어리로 관리하면 좋을 듯함
             // 이것도 상수이면 안됨
             videoData.GetVideoDat(0).gameObject.SetActive(true);
             // 길 안내 변할수 있음
