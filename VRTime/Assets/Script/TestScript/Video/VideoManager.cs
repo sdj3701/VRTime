1d8de591eb494b7f1c0631c9357ad45616ee06de
shindongju <sdj3701@naver.com>
2024-07-01 14:51:43 +0900

    Feat : 비디오 끝나면 활성화
    끝나면 길찾기 다시 활성화 및 끝났다는 이벤트 알려줌

--- VRTime/Assets/Script/TestScript/Video/VideoManager.cs
+++ VRTime/Assets/Script/TestScript/Video/VideoManager.cs
@@ -8,8 +8,7 @@ public class VideoManager : MonoBehaviour
     public GameObject[] videoObject;
     public IVideoData videoData;
     public IPositionable positionable;
-    VideoPlayer videoPlayer;
-    int count = 0;
+    private bool videoPrepared = false;
 
     private void Awake()
     {
@@ -22,34 +21,48 @@ public class VideoManager : MonoBehaviour
             videoData.SetVideoData(i, videoObject[i]);
             videoData.GetVideoData(i).gameObject.SetActive(false);
         }
-        videoPlayer = videoData.GetVideoData(0).GetComponent<VideoPlayer>();
 
     }
-
     private void Update()
     {
-        Debug.Log(videoPlayer.isPlaying);
-        Debug.Log(videoPlayer.name);
-
+        //충돌을 했는지 확인
         if (positionable.GetCheckPoint())
         {
-            videoData.GetVideoData(0).gameObject.SetActive(true);
-            if (count == 0)
-            {
-                videoPlayer.Play();
-                count++;
-            }
-            if (videoPlayer.isPlaying)
+            GameObject videoObject = videoData.GetVideoData(0);
+            videoObject.SetActive(true);
+
+            VideoPlayer videoPlayer = videoObject.GetComponent<VideoPlayer>();
+
+            //영상이 준비 되었는지 이벤트
+            videoPlayer.prepareCompleted += VideoPrepared;
+            videoPlayer.Prepare();
+
+            if (videoPrepared && videoPlayer.isPlaying)
             {
                 Debug.Log("재생 중?");
             }
-            else
+
+            // 비디오가 재생 중이 아닌 경우 처리
+            if (videoPrepared && !videoPlayer.isPlaying)
             {
                 Debug.Log("재생 끝");
-                //false이어야지 길을 찾음
+                // false이어야지 길을 찾음
                 positionable.SetCheckPoint(false);
+
+                // 비디오 준비 완료 이벤트 해제
+                videoPlayer.prepareCompleted -= VideoPrepared;
+
+                // 비디오 게임 오브젝트 비활성화
+                videoPlayer.gameObject.SetActive(false);
             }
-            
+
         }
+
     }
+    private void VideoPrepared(VideoPlayer vp)
+    {
+        videoPrepared = true;
+        vp.Play(); // 준비 완료 후 비디오 재생 시작
+    }
+
 }
