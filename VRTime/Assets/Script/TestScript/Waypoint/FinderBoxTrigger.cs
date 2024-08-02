8518da3c408424e6927d7eb7d14b134d87d6ea3e
sdj3701 <110330701+sdj3701@users.noreply.github.com>
2024-08-02 11:37:19 +0900

    Feat : 손 사라지는것 주석 처리 함

--- VRTime/Assets/Script/TestScript/Waypoint/FinderBoxTrigger.cs
+++ VRTime/Assets/Script/TestScript/Waypoint/FinderBoxTrigger.cs
@@ -45,7 +45,7 @@ public class FinderBoxTrigger : MonoBehaviour
             //videoData.GetVideoData(0).gameObject.SetActive(true);
             // 길 안내 변할수 있음
             positionable.SetCheckPoint(true);
-            if(positionable.GetCheckPoint())
+            /*if(positionable.GetCheckPoint())
             {
                 // true를 사용하면 비활성화된 오브젝트도 검색함
                 Transform[] allChildren = other.gameObject.GetComponentsInChildren<Transform>(true); 
@@ -65,8 +65,8 @@ public class FinderBoxTrigger : MonoBehaviour
                         break;
                     }
                 }
-            }
-            
+            }*/
+
             positionable.SetWayCount(1);
             MyEvent.Invoke(positionable.GetWayCount());
         }
